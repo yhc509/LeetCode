ad4ffd5a7be19721975f048affa4105100aa3bbe
yhc5091 <yhc5091@gmail.com>
2025-02-12 16:58:08 +0900

    Time: 38 ms (42.1%), Space: 71.9 MB (42.11%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cs
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cs
@@ -10,35 +10,24 @@ public class Solution {
     }
 
     public int MaximumSum(int[] nums) {
-        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+        Dictionary<int, int> dict = new Dictionary<int, int>();
 
-        Array.Sort(nums);
-        Array.Reverse(nums);
+        // Array.Sort(nums);
+        // Array.Reverse(nums);
         
+        int max = -1;
         for(int i = 0; i < nums.Length; i++) {
-            var digitSum = SumOfDigit(nums[i]);
-            //Console.WriteLine(nums[i] + "->" + digitSum);
+            var n = nums[i];
+            var digitSum = SumOfDigit(n);
             if(dict.ContainsKey(digitSum) == false) {
-                var l = new List<int>();
-                dict.Add(digitSum, l);
+                dict.Add(digitSum, n);
             }
             else {
-                if(dict[digitSum].Count >= 2) continue;
-            }
-            dict[digitSum].Add(nums[i]);
-        }
-
-        int result = -1;
-
-        foreach(var k in dict) {
-            if(k.Value.Count < 2) continue;
-            int sum = k.Value.Sum();
-            //Console.WriteLine($"{k.Key}, {sum}");
-            if(result <= sum) {
-                result = sum;
+                max = Math.Max(max, dict[digitSum] + n);
+                dict[digitSum] = Math.Max(dict[digitSum], n);
             }
         }
 
-        return result;
+        return max;
     }
 }
