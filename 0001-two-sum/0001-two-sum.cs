be01e851bee53554f3c38c1e2b1561489aaebe09
yhc5091 <yhc5091@gmail.com>
2025-02-13 12:55:42 +0900

    Time: 94 ms (5.16%), Space: 48.6 MB (36.71%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for(int i = 0; i < nums.Length; i++) {
+            for(int j = 0; j < nums.Length; j++) {
+                if(i == j) continue;
+
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
